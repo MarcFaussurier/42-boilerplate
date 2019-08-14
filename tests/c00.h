63b20ba19e8e286eae060f8106d72d8338a7215a
Marc Faussurier <mfaussur@z2r1p3.le-101.fr>
2019-08-14 20:50:04 +0200

    add test 03

--- tests/c00.h
+++ tests/c00.h
@@ -18,6 +18,7 @@ void	test_ft_putchar()
 		if (c == 127)
 			break ;
 	}
+	//write(1, "o", 1);
 }
 void	true_function()
 {
@@ -27,7 +28,7 @@ void	true_function()
 	while (1)
 	{
 		c += 1;
-		printf("%c", c);
+		write(1, &c, 1);
 		if (c == 127)
 			break ;
 	}
@@ -103,6 +104,205 @@ MU_TEST(t_putnbr)
 	mu_assert_string_eq(ob(putnbr), ob(putnbr_true));
 }
 
+void	display_comb(char n1, char n2, char n3)
+{
+	if (n1 + n2 + n3 != '0' + '1' + '2')
+	{
+		write(1, ",", 1);
+		write(1, " ", 1);
+	}
+	write(1, &n1, 1);
+	write(1, &n2, 1);
+	write(1, &n3, 1);
+}
+
+void	print_comb_true(void)
+{
+	char	n1;
+	char	n2;
+	char	n3;
+
+	n1 = '0' - 1;
+	n2 = '1' - 1;
+	n3 = '2' - 1;
+	while (++n1 <= '7')
+	{
+		while (++n2 <= '8')
+		{
+			while (++n3 <= '9')
+			{
+				if (n1 < n2 && n2 < n3)
+				{
+					display_comb(n1, n2, n3);
+				}
+			}
+			n3 = '0';
+		}
+		n2 = '0';
+	}
+}
+
+MU_TEST(t_print_comb)
+{
+	mu_assert_string_eq(ob(ft_print_comb), ob(print_comb_true));
+}
+
+void	ft_print_comb2_numbers_true(unsigned short n1, unsigned short n2)
+{
+	char out1;
+	char out2;
+
+	if (!(n1 == 0 && n2 == 1))
+	{
+		out1 = ',';
+		write(1, &out1, 1);
+		out1 = ' ';
+		write(1, &out1, 1);
+	}
+	out2 = '0' + n1 % 10;
+	n1 /= 10;
+	out1 = '0' + n1 % 10;
+	write(1, &out1, 1);
+	write(1, &out2, 1);
+	out1 = ' ';
+	write(1, &out1, 1);
+	out2 = '0' + n2 % 10;
+	n2 /= 10;
+	out1 = '0' + n2 % 10;
+	write(1, &out1, 1);
+	write(1, &out2, 1);
+}
+
+void	ft_print_comb2_true(void)
+{
+	unsigned short n1;
+	unsigned short n2;
+
+	n1 = 0;
+	n2 = 1;
+	while (n1 <= 98)
+	{
+		while (n2 <= 99)
+		{
+			if (n1 < n2)
+			{
+				ft_print_comb2_numbers_true(n1, n2);
+			}
+			n2 += 1;
+		}
+		n2 = 0;
+		n1 += 1;
+	}
+	n1 = 0;
+}
+
+MU_TEST(t_print_comb2)
+{
+	mu_assert_string_eq(ob(ft_print_comb2), ob(ft_print_comb2_true));	
+}
+
+
+int		ft_check_combn_digits_true(short *digits, int n)
+{
+	int	x;
+
+	x = n - 1;
+	while (x > 0)
+	{
+		if (digits[x - 1] >= digits[x])
+		{
+			digits[x] = digits[x - 1];
+			return (0);
+		}
+		x -= 1;
+	}
+	return (1);
+}
+
+void	ft_print_combn_digits_true(short *digits, int n, int isfirst)
+{
+	int		x;
+	char	out;
+
+	x = 0;
+	if (!isfirst)
+	{
+		write(1, ", ", 2);
+	}
+	while (x < n)
+	{
+		out = digits[x] + '0';
+		write(1, &out, 1);
+		x += 1;
+	}
+}
+
+void	ft_increment_combn_digits_true(short *digits, int n, int current_digit)
+{
+	digits[current_digit] += 1;
+	if (digits[current_digit] == 10)
+	{
+		digits[current_digit] = 0;
+		ft_increment_combn_digits_true(digits, n, current_digit - 1);
+	}
+	else
+	{
+		return ;
+	}
+}
+
+int		ft_sum_combn_digits_true(short *digits, int n)
+{
+	int		x;
+	int		sum;
+	int		ten;
+
+	ten = 1;
+	sum = 0;
+	x = n - 1;
+	while (x >= 0)
+	{
+		sum += ten * digits[x];
+		x -= 1;
+		ten *= 10;
+	}
+	return (sum);
+}
+
+void	ft_print_combn_true(int n)
+{
+	short	digits[n];
+	short	max_digits[n];
+	int		x;
+	int		max_sum;
+	int		current_sum;
+
+	x = 0;
+	while (x < n)
+	{
+		digits[x] = x;
+		max_digits[x] = (10 - n + x);
+		x += 1;
+	}
+	max_sum = ft_sum_combn_digits_true(max_digits, n);
+	ft_print_combn_digits_true(digits, n, 1);
+	current_sum = 0;
+	while (current_sum < max_sum)
+	{
+		ft_increment_combn_digits_true(digits, n, n - 1);
+		if (ft_check_combn_digits_true(digits, n))
+		{
+			ft_print_combn_digits_true(digits, n, 0);
+			current_sum = ft_sum_combn_digits_true(digits, n);
+		}
+	}
+}
+
+MU_TEST(t_print_combn)
+{
+	mu_assert_string_eq(ob(ft_print_combn), ob(ft_print_combn_true));
+}
+
 MU_TEST_SUITE(test_suite)
 {
 	MU_RUN_TEST(t_ft_putchar);
@@ -110,7 +310,10 @@ MU_TEST_SUITE(test_suite)
 	MU_RUN_TEST(t_ft_print_reverse_alphabet);
 	MU_RUN_TEST(t_ft_print_numbers);
 	MU_RUN_TEST(t_ft_is_negative);
+	MU_RUN_TEST(t_print_comb);
 	MU_RUN_TEST(t_putnbr);
+	MU_RUN_TEST(t_print_comb2);
+	MU_RUN_TEST(t_print_combn);
 }
 
 void	testc00()
---
+++ tests/c03.h
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   c03.h                                            .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mfaussur <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/08/14 13:34:29 by mfaussur     #+#   ##    ##    #+#       */
+/*   Updated: 2019/08/14 20:44:12 by mfaussur    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "../libs/minunit/minunit.h"
+#include "../includes/c03.h"
+
+
+MU_TEST(t_ft_strcmp)
+{
+	mu_assert(ft_strcmp("abcd", "abcde") ==  strcmp("abcd", "abcde"), "strncmp compares pas n");
+	mu_assert(ft_strcmp("abc", "abe") ==  strcmp("abc", "abe"), "strncmp fails to compare n-1st first");
+}
+
+
+MU_TEST(t_ft_strncmp)
+{
+	mu_assert(ft_strncmp("abcd", "abcde", 3) ==  0, "strncmp compares pas n");
+	mu_assert(ft_strncmp("abc", "abe", 3) ==  -2, "strncmp fails to compare n-1st first");
+}
+
+MU_TEST(t_ft_strcat)
+{
+	char b[32];
+	strcpy(b, "abc");
+	mu_assert_string_eq(ft_strcat(b, "123456"), "abc123456");
+	mu_assert(b[9] == 0, "strcat dont fill with trailling nop");
+}
+
+MU_TEST(t_ft_strncat)
+{
+	char b[32];
+	char c[32];
+	strcpy(b, "abc");
+	strcpy(c, "abc");
+	mu_assert_string_eq(strncat(b, "123456", 3), ft_strncat(c, "123456", 3));
+	mu_assert(b[6] == c[6] && b[6] == 0, "error missing trailling null");
+	strcpy(b, "abc");
+	strcpy(c, "abc");
+	mu_assert_string_eq(strncat(b, "123456", 0), ft_strncat(c, "123456", 0));
+	strcpy(b, "abc");
+	strcpy(c, "abc");
+	mu_assert_string_eq(strncat(b, "123456", 12), ft_strncat(c, "123456", 12));
+}
+
+MU_TEST(t_ft_strstr)
+{
+	mu_assert_string_eq(strstr("avion", "vi"), ft_strstr("avion", "vi"));
+	mu_assert_string_eq(strstr("abcd", "a"), ft_strstr("abcd", "a"));
+	mu_assert_string_eq(strstr("avion", ""), ft_strstr("avion", ""));
+	mu_assert_string_eq(strstr("", "vi"), ft_strstr("", "vi"));
+}
+
+MU_TEST(t_ft_strlcat)
+{
+	char b[33];
+	char c[33];
+	char	buffer[255];
+	char	buffer2[255];
+
+	memcpy(b, "abc\0\0\0x\0", 8);
+	memcpy(c, "abc\0\0\0x\0", 8);
+	sprintf(buffer, "%u", ft_strlcat(c, "123", 6));
+	sprintf(buffer2, "%lu", strlcat(b, "123", 6));
+	mu_assert_string_eq(buffer2, buffer);
+
+	memcpy(b, "abc\0\0\0x\0", 8);
+	memcpy(c, "abc\0\0\0x\0", 8);
+	sprintf(buffer, "%u", ft_strlcat(c, "123", 4));
+	sprintf(buffer2, "%lu", strlcat(b, "123", 4));
+	mu_assert_string_eq(buffer2, buffer);
+
+	memcpy(b, "abc\0\0\0x\0", 8);
+	memcpy(c, "abc\0\0\0x\0", 8);
+	sprintf(buffer, "%u", ft_strlcat(c, "123", 8));
+	sprintf(buffer2, "%lu", strlcat(b, "123", 8));
+	mu_assert_string_eq(buffer2, buffer);
+
+	memcpy(b, "abc\0\0\0x\0", 8);
+	memcpy(c, "abc\0\0\0x\0", 8);
+	sprintf(buffer, "%u", ft_strlcat(c, "123", 8));	
+	sprintf(buffer2, "%lu", strlcat(b, "123", 8));
+	mu_assert_string_eq(buffer2, buffer);
+	
+	memcpy(b, "im", 8);
+	memcpy(c, "a str", 8);
+	int got = ft_strlcat(b, c, 7);
+	sprintf(buffer, "%u", got);
+	
+	memcpy(b, "im", 8);
+	memcpy(c, "a str", 8);
+	int ori = strlcat(b, c, 7);
+	sprintf(buffer2, "%u", ori);
+	mu_assert_string_eq(buffer2, buffer);
+	mu_assert(ori == got, "error in strlcat return statement");
+	
+	memcpy(b, "abc\0\0\0x\0", 8);
+	memcpy(c, "abc\0\0\0x\0", 8);
+	sprintf(buffer, "%u", ft_strlcat(c, "123", 3));
+	sprintf(buffer2, "%lu", strlcat(b, "123", 3));
+	mu_assert_string_eq(buffer2, buffer);
+}
+
+MU_TEST_SUITE(test_suite_3)
+{
+	MU_RUN_TEST(t_ft_strcmp);
+	MU_RUN_TEST(t_ft_strncmp);
+	MU_RUN_TEST(t_ft_strcat);
+	MU_RUN_TEST(t_ft_strncat);
+	MU_RUN_TEST(t_ft_strstr);
+	MU_RUN_TEST(t_ft_strlcat);
+}
+
+void	testc03()
+{
+
+	MU_RUN_SUITE(test_suite_3);
+}
+
